c81a7007288335ba73323c92cf49ca484cdc1e19
jokerxin <jxpro@qq.com>
2024-12-26 16:59:32 +0800

    Add note on type inference

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     // let args_turbofish = env::args().collect::<Vec<String>>();
     // dbg!(args_turbofish);
 
-    let args: Vec<String> = env::args().collect();
+    // 可以简单使用 _ 占位符代替具体的 String 类型，但是 Vec<_> 不能省略，因为还可能是 HashSet<_> 等其他类型
+    let args: Vec<_> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
